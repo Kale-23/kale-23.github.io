0130b7276a89316b5c99776cba6b57643583e1a0
Kale-23 <kalebduch18@gmail.com>
2024-05-22 22:55:13 -0400

    try13

--- src/routes/pic/+page.svelte
+++ src/routes/pic/+page.svelte
@@ -4,6 +4,7 @@
     }
 </script>
 
+<a href="/">Home</a>
 <h1> puppy </h1>
 <button on:click={handleClick}> Click Me </button>
 <img src="https://vetmed.tamu.edu/news/wp-content/uploads/sites/9/2023/05/AdobeStock_472713009.jpeg" alt="idk something">
