53c6429ae660dad893a6ae3a197fb0fd97fc8038
Kale-23 <kalebduch18@gmail.com>
2024-05-23 00:01:48 -0400

    try19

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : "/" //process.env.BASE_PATH
+			base: process.argv.includes('dev') ? '' : "/test" //process.env.BASE_PATH
 		}
 	}
 };
