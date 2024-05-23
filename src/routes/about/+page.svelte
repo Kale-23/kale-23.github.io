5e062605a44e0c1a3f847b6b72667b3ebd8bf360
Kale-23 <kalebduch18@gmail.com>
2024-05-22 20:52:11 -0400

    initial commit

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,12 @@
+<h1> hello </h1>
+<div class="about">
+    <h2> About Me </h2>
+    <p> I am bioinformatician </p>
+</div>
+
+<style>
+    .about {
+        text-align: center;
+        color: blue;
+    }
+</style>
---
+++ src/routes/pic/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    const handleClick = () => {
+        alert("puppy")
+    }
+</script>
+
+<h1> puppy </h1>
+<button on:click={handleClick}> Click Me </button>
+<img src="https://vetmed.tamu.edu/news/wp-content/uploads/sites/9/2023/05/AdobeStock_472713009.jpeg" alt="idk something">
+
+<style>
+    h1 {
+        color: blue;
+    }
+</style>
