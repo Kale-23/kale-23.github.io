e4721170aa9ca44997fa1a4d3bdf9be79af64650
Kale-23 <kalebduch18@gmail.com>
2024-05-23 00:28:43 -0400

    try22

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    process.argv.includes('dev') ? '' : "/test/",
 	plugins: [sveltekit()]
 });
